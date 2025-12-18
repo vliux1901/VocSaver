f1284789f72d9b8f81a0a56cf8b617b665493da5
Xin Liu <vliux@xinliu-t14-6.lan>
2025-12-17 22:24:15 -0800

    Shared texts are now saved to DB. Also set up the missing room compiler, so db implementation is now generated.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -51,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/vliux/vocsaver/MainActivity.kt
+++ app/src/main/java/com/vliux/vocsaver/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vliux.vocsaver
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,15 +13,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.vliux.vocsaver.data.AppDatabase
+import com.vliux.vocsaver.data.Word
+import com.vliux.vocsaver.data.WordRepository
 import com.vliux.vocsaver.ui.theme.VocSaverTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy { AppDatabase.getDatabase(this) }
+    private val repository by lazy { WordRepository(db.wordDao()) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val sharedText = when (intent?.action) {
             Intent.ACTION_SEND -> intent.getStringExtra(Intent.EXTRA_TEXT)
             else -> null
+        }
+        if (sharedText != null) {
+            lifecycleScope.launch {
+                try {
+                    repository.insert(Word(word = sharedText, added_ts = System.currentTimeMillis(), translation = null))
+                    Toast.makeText(this@MainActivity, "Saved", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, "Failed to save: $e", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
         setContent {
             VocSaverTheme {
